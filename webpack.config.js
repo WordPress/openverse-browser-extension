5c5f94a94fd3f5cfa2b6f4baa78db453d6b022e5
Dhruv Bhanushali <dhruv_b@live.com>
2021-08-04 14:38:42 +0530

    Remove Firefox extras

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
-const firefoxExtra = require('./src/manifest_extra.firefox.json');
-
 const target = process.env.TARGET;
 if (target && ['chrome', 'firefox', 'opera', 'edge'].includes(target)) {
   console.info(`\x1b[1;32mBuilding for ${process.env.TARGET}...\x1b[m`);
@@ -30,15 +28,6 @@ module.exports = {
       {
         from: `./manifest.json`,
         to: './manifest.json',
-        transform(content) {
-          if (target === 'firefox') {
-            let manifest = JSON.parse(content.toString());
-            manifest = { ...manifest, ...firefoxExtra };
-            return JSON.stringify(manifest, null, 2);
-          }
-
-          return content;
-        },
       },
       {
         from: './icons/*',
